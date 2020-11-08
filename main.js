540d7c17698d3927a541ce86c4e654690c1c6b4f
FrancescoDale <fd060862@gmail.com>
2020-11-08 16:13:47 +0100

    parte principale

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // faccio scegliere che livello di difficoltà avrà il gioco
 var gameDiff = 0; // preimposto a 0
 var gameNumbers;
-var randomNumbers = [];
+var randomNumbers = []; // mine
+var inputNumbers = [];
+var gameOver = false; //variabile binaria che a seconda del valore decide quando il gioco finisce
 
 do{
     gameDiff = parseInt(prompt('Scegli la difficoltà : da 0 ( minimo ) a 2 ( massimo ):  '));
@@ -26,7 +28,7 @@ else if(gameDiff==2) {
 //controllo
 console.log(' caselle campo minato = ' + gameNumbers);
 
-// genero dei numeri casuali
+// genero dei numeri casuali ( mine )
 while(randomNumbers.length < 16) {  // 15 numeri
     var r = Math.floor((Math.random() * gameNumbers)+1); // genera il numero e lo assegna come valore alla variabile r
     if(randomNumbers.indexOf(r) === -1) randomNumbers.push(r); // push solo quando è diverso
@@ -34,3 +36,13 @@ while(randomNumbers.length < 16) {  // 15 numeri
 
 //controllo
 console.log(randomNumbers);
+
+//gioco vero e proprio
+
+// ciclo che si interrompe solo quando gameOver assume valore true
+
+while (gameOver==true) {
+    console.log(' Game Over!')
+
+    
+}
