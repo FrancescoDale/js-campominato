4459ee0a9a3bd5e3f2cf9920b736cc5004ae0531
FrancescoDale <fd060862@gmail.com>
2020-11-08 15:36:32 +0100

    genero 15 numeri casuali

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // faccio scegliere che livello di difficoltà avrà il gioco
 var gameDiff = 0; // preimposto a 0
 var gameNumbers;
+var randomNumbers = [];
 
 do{
     gameDiff = parseInt(prompt('Scegli la difficoltà : da 0 ( minimo ) a 2 ( massimo ):  '));
@@ -8,7 +9,6 @@ do{
 } while(gameDiff>2 || gameDiff<0)  // finchè nel prompt non vengono inseriti valori che rispettano questa condizione, il ciclo si ripete
 
 // controllo
-
 console.log('Hai scelto : ' + gameDiff);
 
 // a seconda del valore della variabile gameDiff, il campo minato assume diverse dimensioni
@@ -22,3 +22,15 @@ else if(gameDiff==1) {
 else if(gameDiff==2) {
         gameNumbers = 50;
     }
+
+//controllo
+console.log(' caselle campo minato = ' + gameNumbers);
+
+// genero dei numeri casuali
+while(randomNumbers.length < 16) {  // 15 numeri
+    var r = Math.floor((Math.random() * gameNumbers)+1); // genera il numero e lo assegna come valore alla variabile r
+    if(randomNumbers.indexOf(r) === -1) randomNumbers.push(r); // push solo quando è diverso
+}
+
+//controllo
+console.log(randomNumbers);
